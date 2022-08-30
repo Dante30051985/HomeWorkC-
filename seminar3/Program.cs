4b168d6117deda0f6c48ba86f9b98f530ce39c59
Artem Shubin <artem.shubin2010@mail.ru>
2022-08-30 19:12:49 +0500

    Практические работы семинары по программированию C#

---
+++ seminar3/Program.cs
@@ -0,0 +1,93 @@
+﻿/*Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+ void StartProgram(int num)
+ {
+     //Определяю разрядность введённого числа округляю вниз до целой части числа Log10(num)
+     int CheckingBitDepth=1+(int)Math.Log10(num); 
+     if (CheckingBitDepth < 5 | CheckingBitDepth > 5) {Console.Write("Вы не ввели пятизначное число! ");}
+     else
+     {
+         //определяем первую, вторую, предпоследнюю и последнюю цифру входного числа
+         int a1 = (num % 100000) / 10000 ; 
+         int a2 = (num % 10000) / 1000; 
+         int a3 = (num % 100) / 10; 
+         int a4 = (num % 10); 
+         //сравниваеи первую, вторую, предпоследнюю и последнюю цифру входного числа
+         if (a1 == a4 & a2 == a3) {Console.WriteLine("Данное число является палиндромом.");}
+         else {Console.WriteLine("Число не является палиндромом");}
+     }  
+ }
+
+ while(true)
+ {
+ Console.Write("Введите пятизначное число(для выхода введите exit): ");
+ string text=Console.ReadLine();
+ if (text.Equals("exit")) {Console.Clear(); System.Environment.Exit(0);}
+ if (int.TryParse(text, out int num))
+ { StartProgram(num);} else {Console.WriteLine("Вы не ввели число! Введите пожалуйста пятизначное число. Для его проверки является ли оно палиндромом");}
+ }
+ Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+Изменяю представление пользовательского языка системы по умолчанию так как представление дробного числа по умолчанию ",",
+а мне нужна ".
+*/
+/*Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+//Изменяю локацию культуры, региона (так как в русскоязычной локации, в числе с плавающей точкой проставляется , а нужна .)
+
+Console.WriteLine("Введите координаты двух точек А и В лежащих в 3D пространстве по осям X,Y,Z. ");
+//обьявляю массивы где буду храниться введёные значения пользователем 
+double[] pointA=new double[3]; //[0] - x, [1] - y, [2] - z
+double[] pointB=new double[3];
+//Вызываю метод в котором пользователь вводит координаты точек
+CompletionArray(pointA,pointB);
+//Вызываю функцию расчёта расстояние между точками
+double ResultDistance=Distance(pointA, pointB);
+//результат 
+Console.WriteLine("Расстояние между точками в 3D пространстве составляет: "+ResultDistance);
+//метод заполнения обьявленных ранее массивов
+void CompletionArray(double[] pointA,double[] pointB )
+{
+    Console.Clear(); //очищаю консоль
+string RequestEnterPointA="Введите через запятую, координаты точки А лежащей на X,Y,Z(для ввода дробного числа используйте .): ";
+Console.Write(RequestEnterPointA);
+string[] EnterPointA = Console.ReadLine().Split(','); //каждый новый элемент массива EnterPointA будет после ","
+int SizeArrayA=EnterPointA.Length; //измеряю массив введённых значений
+    for (int i=0;i<SizeArrayA;i++) //проход циклом for
+        {
+            //проверю тип введёных данных если всё нормально пишу в массив XYZpointA  
+            if (double.TryParse(EnterPointA[i], out double XYZpointA)) { 
+                 pointA[i]=XYZpointA;
+        } }
+string RequestEnterPointB="Введите через запятую, координаты точки В лежащей на X,Y,Z(для ввода дробного числа используйте .): ";
+Console.Write(RequestEnterPointB);
+string[] EnterPointB = Console.ReadLine().Split(',');
+int SizeArrayB=EnterPointB.Length;
+    for (int i=0;i<SizeArrayB;i++)
+    {
+            if (double.TryParse(EnterPointB[i], out double XYZpointB)) { 
+                    pointB[i]=XYZpointB;
+        }
+    } }
+//функция расчёта расстояния между точками А и В
+double Distance(double[] pointA, double[] pointB)
+{
+    double result; //обьяляю переменную которая будет хранить результат
+    //формула расчёт расстояния между точкаи, использую Math класс имеющийся в C#
+    result=Math.Sqrt(Math.Pow(pointB[0]-pointA[0],2)+Math.Pow(pointB[1]-pointA[1],2)+Math.Pow(pointB[2]-pointA[2],2) );
+    return result; //возвращаю результат
+}*/
+
+/*Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Здравствуйте! Введите число N: ");
+int numberN=Convert.ToInt32(Console.ReadLine()); //считываю значения с клавиатуры
+
+StartProgram(numberN);
+void StartProgram(int numberN)
+{
+    int count=1; //ставлю счётчик от 1
+    while (count <=numberN) //если счётчик  меньше или равен введёному числу, то конец цикла
+        {
+            int rezult =(count*count*count); //перемножаю три раза count само на себя
+            Console.WriteLine(rezult);
+            count++; //увеличиваю значение счётчика
+        }
+}*/
---
+++ seminar4/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int StartProgram(int[] enterNum)
+{
+   int number_a=enterNum[0]; //число A
+   int natural_degre=enterNum[1]; //натуральная степень В
+   int result=enterNum[0]; //храню результат
+
+    //прохожу циклом от 1 до ввёдной степени
+   for (int curr=1;curr<natural_degre;curr++)
+   {    
+        result=result*number_a;
+   }
+    return result;
+}
+Console.Clear();
+Console.Write("Введите через запятую, число А и его степень(число В): ");
+//записываю в массив введённые значение в консоли, разделителем переменных служит запятая
+//выбираю преобразованное строковое значение из консоли, в представлении числа, и заполняю массив enterNum
+int[] enterNum=Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+int result=StartProgram(enterNum);
+Console.WriteLine($"Число {enterNum[0]} в степени {enterNum[1]}, будет иметь ответ:{result}");
+*/
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SummaNumber(int enterNum)
+{
+    int result=0; //храню результат
+
+    //прохожу циклом если введённое число не равно нулю
+    //введёное число делю на 10 для поиска 
+    остатка
+    //разделёное число возвращается в цикл и снова делится на 10
+    for (; enterNum != 0; enterNum/=10) { result += enterNum % 10; }
+   
+    return result;
+}
+
+Console.Write("Введите любое двух, трёхзначное число и посчитаю сумму его цифр: ");
+int enterNum=Convert.ToInt32(Console.ReadLine());
+int result=SummaNumber(enterNum);
+Console.WriteLine(result);*/
+
+//Задача 29: Напишите программу, которая задаёт массив из произвольного элементов и выводит их на экран.
+ int[] ArrayInit(int enterNum)
+ {
+   int[] array=new int[enterNum]; //задаю размер массива введённым знчением с клавиатуры
+  
+ //  прохожу циклом и заполняю массив array
+  for (int i=0;i<enterNum;i++)
+  {
+      Console.WriteLine($"Элемент {i} значение:");
+      array[i]=int.Parse(Console.ReadLine()); //преобразую строку в int
+  }
+    return array;
+}
+
+ void OutScreenArray(int[] array)
+ {
+ Console.Write("Массив содержит элементы:[");
+    for (int i=0;i<array.Length;i++) //прохожу циклом по массиву и вывожу его значения
+    {   Console.Write(" "+array[i]+" ");   }
+Console.Write(" ]");
+ }
+
+Console.Write("Введите число, для задания массива: ");
+int enterNum=int.Parse(Console.ReadLine());
+//вызов метода вывод массива на экран(в нём в качестве параметра передаю что сначала вызвать функцию заполнения массива)
+OutScreenArray(ArrayInit(enterNum)); 
