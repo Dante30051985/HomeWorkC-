2a497bc17dd2d1cc7f524769c540b785ded5657a
Artem Shubin <110925660+Dante30051985@users.noreply.github.com>
2022-09-29 12:53:30 +0300

    Update Program.cs

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -1,4 +1,4 @@
-﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами.
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
@@ -9,6 +9,32 @@ StartProgram();
 
 void StartProgram()
 {
-    Console.Write("Введите любые слова:"+"/t");
-    
+    Console.Clear();
+    Console.Write("Введите любые слова:"+"\t");
+    string[] enterArray = Console.ReadLine().Split(",").ToArray();
+    PrintEnterArray(enterArray);
+    Console.WriteLine();
+    string[] result= NewArray(enterArray);
+    PrintResult(result);
+}
+
+string[] NewArray(string[] enterArray)
+{
+    string[] narray=new string[enterArray.Length];
+    for (int i=0;i<enterArray.Length;i++)
+        { if (enterArray[i].Length<=3)
+                { narray[i] = enterArray[i]; } }
+    return narray;
+}
+
+void PrintResult(string[] result)
+{
+    for (int i=0;i<result.Length;i++)
+    { Console.Write("['"+result[i]+"']"+"\t"); }    
+}
+
+void PrintEnterArray(string[] enterArray)
+{
+     for (int i=0;i<enterArray.Length;i++)
+        { Console.Write("['"+enterArray[i]+"']"+"\t");}
 }
